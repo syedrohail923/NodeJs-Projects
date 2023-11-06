094197dd8103a82de774380805ffe14588ca8594
syedrohail923 <143003070+syedrohail923@users.noreply.github.com>
2023-11-07 04:36:24 +0500

    Add files via upload

---
+++ word-counter/index.ts
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+
+async function startLoop(){
+    do{
+        await getSentence();
+        var again = await inquirer.prompt([
+            {
+                type: "list",
+                name: "loop",
+                choices: ["Yes", "No"],
+                message: "Do you want to continue: "
+            }
+        ]);
+    }while(again.loop=="Yes");
+}
+
+startLoop();
+
+function wordCount(sent: string): number{
+    if(sent.length>0){
+        const words = sent.split(" ");
+        console.log(words);
+        return words.length;
+    }else{
+        return 0;
+    }
+}
+
+async function getSentence(){
+    const answer: {
+        sentence: string
+    } = await inquirer.prompt([
+        {
+            type: "input",
+            name: "sentence",
+            message: "Please write your sentence to count words: "
+        }
+    ]);
+    console.log(`Word count in your sentence is ${wordCount(answer.sentence.trim())}`);
+}
